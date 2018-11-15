472608f8a18c2d06ddc411bcd60cd7d9be681520
94natem <nate_m@live.com>
2018-11-15 15:21:08 -0700

    JS with updated and working reset function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,38 @@
-    var counter = 0;
-    var wins = 0;
-    var losses = 0;
-    var computerScore = "";
-    var userScore = "";
-    var crystalImages = ["assets/images/blue.png", "assets/images/green.png", "assets/images/red.png", "assets/images/yellow.png"]
+$(document).ready( function (){
 
-    var computerScore = Math.floor(Math.random() * (120-19) + 19) 
+var counter = 0;
+var wins = 0; //# of succesful matches
+var losses = 0; //# of times counter > computerscore
+var computerScore = "";
+var roundFinished = false
+var crystalImages = ["assets/images/blue.png", "assets/images/green.png", "assets/images/red.png", "assets/images/yellow.png"] //array containing crystal images
+
+
+
+// Reset round function
+function roundStarted() {
+    roundFinished = false;
+    counter = 0
+    computerScore = Math.floor(Math.random() * (120-19) + 19) 
     $("#goalScore").html("Goal Score:" + computerScore);
+    $("#counter").html("Your Score:" + counter)
+    console.log(computerScore);
+}
+   
+//Generate random number for the goalscore on window open
 
+var computerScore = Math.floor(Math.random() * (120-19) + 19) 
+$("#goalScore").html("Goal Score:" + computerScore);
+console.log(computerScore);
 
-$(".btn-dark").on("click", function () {
-    counter=0
     
-    $("#counter").html("Your Score:" + counter);
-    
-    var computerScore = Math.floor(Math.random() * (120-19) + 19) 
-    
-    $("#goalScore").html("Goal Score:" + computerScore);
-})
-    
+
+//Attach images to the page and assign a random value between 1-12 to each image.    
 crystalImages.forEach(function (imageUrl, index) {
         
     var crystal = $("<img>");
     
-    crystal.attr("src", imageUrl)
+    crystal.attr("src", imageUrl);
 
     var randomVal = Math.floor(Math.random() * 12) + 1;
 
@@ -36,6 +45,7 @@ crystalImages.forEach(function (imageUrl, index) {
     $("#images").append(crystal);
 });
 
+//Click function on each image /Add each crystals value to the score counter
 $("#images").on("click", ".crystal", function () {
         
     var crystalVal = $(this).attr("data-points");
@@ -43,20 +53,24 @@ $("#images").on("click", ".crystal", function () {
     var crystalValInt = parseInt(crystalVal);
         
     counter = counter + crystalValInt;
-        
     $("#counter").html("Your Score:" + counter);
 
-    if (counter === computerScore) {
-        
+//win and loss conditions and calling reset round function
+if (counter === computerScore) {
         wins++;
-        
         $("#wins").html("Wins:" + wins);
-    
+        roundFinished = true;
+        roundStarted();
+        
     } else if (counter > computerScore) {
-        
         losses++;
-        
         $("#losses").html("losses:" + losses);
+        roundFinished = true;
+        roundStarted();
+        
     }
+});
+
 })
+
 
